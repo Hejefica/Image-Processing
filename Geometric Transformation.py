ccec5f1057de6176b5870bccc460eb9879d28c5a
Héctor Figueroa <69779567+Hejefica@users.noreply.github.com>
2022-05-18 13:39:07 -0500

    .

--- Geometric Transformation.py
+++ Geometric Transformation.py
@@ -2,49 +2,48 @@ import numpy as np
 import cv2
 from ImgResize import image_resize
 
-imgori = cv2.imread("CETI.jpg")
+Img = cv2.imread("CETI.jpg")
 
-def resize(imagen, ancho, alto):
-    dim = (ancho, alto)
-    img_resized = cv2.resize(imagen, dim, interpolation = cv2.INTER_AREA)
-    return img_resized
+def resize(Img, Width, Height):
+    dim = (Width, Height)
+    ResizedImg = cv2.resize(Img, dim, interpolation = cv2.INTER_AREA)
+    return ResizedImg
 
-def translation(imagen, desp_x, desp_y):
-    dimension_original = imagen.shape
+def Traslation(Img, X, Y):
+    OriginalShape = Img.shape
     M = np.float32([
-         [1, 0, desp_x],
-         [0, 1, desp_y]
+         [1, 0, X],
+         [0, 1, Y]
          ])
-    img_trasl = cv2.warpAffine(imagen, M, (dimension_original[1], dimension_original[0]))
-    return img_trasl
+    TraslatedImg = cv2.warpAffine(Img, M, (OriginalShape[1], OriginalShape[0]))
+    return TraslatedImg
 
-def rotation (imagen, angulo):
-    altura, ancho = imagen.shape[:2]
-    centro = (altura/2, ancho/2)
-    matriz_rotacion = cv2.getRotationMatrix2D(center=centro, angle=angulo, scale=1)
-    rotated_image = cv2.warpAffine(imgori, M=matriz_rotacion, dsize=(ancho, altura))
-    return rotated_image
+def Rotation (Img, Angle):
+    Height, Width = Img.shape[:2]
+    Center = (Height/2, Width/2)
+    RotationMatrix = cv2.getRotationMatrix2D(center=Center, angle=Angle, scale=1)
+    RotatedImg = cv2.warpAffine(Img, M=RotationMatrix, dsize=(Width, Height))
+    return RotatedImg
 
-def flip(imagen, eje):
-    img_volteada = cv2.flip(imagen, eje)
-    return img_volteada    
+def flip(Img, Axis):
+    FlippedImg = cv2.flip(Img, Axis)
+    return FlippedImg    
 
-img_resized = resize(imgori, 300, 240)
-img_trasl = translation(imgori, 56, 20)
-img_rotated = rotation(imgori, -12)
-img_flipped = flip(imgori, 1)
-
+ResizedImg = resize(Img, 300, 240)
+TraslatedImg = Traslation(Img, 56, 20)
+RotatedImg = Rotation(Img, -12)
+FlippedImg = flip(Img, 1)
 
 ImgSize = 220
-cv2.imshow("Original Image", image_resize(imgori, Height = ImgSize))
+cv2.imshow("Original Image", image_resize(Img, Height = ImgSize))
 cv2.moveWindow('Original Image', 20, 20)
-cv2.imshow("Resized Image", image_resize(img_resized, Height = ImgSize))
+cv2.imshow("Resized Image", image_resize(ResizedImg, Height = ImgSize))
 cv2.moveWindow('Resized Image', 300, 20)
-cv2.imshow("Traslated Image", image_resize(img_trasl, Height = ImgSize))
+cv2.imshow("Traslated Image", image_resize(TraslatedImg, Height = ImgSize))
 cv2.moveWindow('Traslated Image', 580, 20)
-cv2.imshow("Rotated Image", image_resize(img_rotated, Height = ImgSize))
+cv2.imshow("Rotated Image", image_resize(RotatedImg, Height = ImgSize))
 cv2.moveWindow('Rotated Image', 860, 20)
-cv2.imshow("Fliped Image", image_resize(img_flipped, Height = ImgSize))
+cv2.imshow("Fliped Image", image_resize(FlippedImg, Height = ImgSize))
 cv2.moveWindow('Fliped Image', 1140, 20)
 
 cv2.waitKey(0)
